acab59ea2814a8db91f4f976adc00b5fe08dbd12
vertoker <vertoker@gmail.com>
2026-03-14 20:22:47 +0500

    start lifetime scope

---
+++ Assets/Scripts/GameScope.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+public class GameScope : LifetimeScope
+{
+    protected override void Configure(IContainerBuilder builder)
+    {
+        Debug.Log($"{nameof(GameScope)} is configured");
+    }
+}
